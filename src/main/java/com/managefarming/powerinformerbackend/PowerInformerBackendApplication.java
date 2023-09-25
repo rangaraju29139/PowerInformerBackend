74ac4972e405860290159bf8ff46b4e5f08e4b99
rangaraju penmetsa <rangaraju29139@gmail.com>
2023-09-25 23:15:47 +0530

    initial commit

---
+++ src/main/java/com/managefarming/powerinformerbackend/PowerInformerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.managefarming.powerinformerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerInformerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PowerInformerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/managefarming/powerinformerbackend/PowerInformerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.managefarming.powerinformerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowerInformerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
