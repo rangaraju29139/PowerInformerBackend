e99b1d0e591ecbf300947f8ee24ec07970777a40
rangaraju penmetsa <rangaraju29139@gmail.com>
2023-10-01 23:40:48 +0530

    added the simple home page endpoint

--- src/main/java/com/managefarming/powerinformerbackend/controllers/PowerInformerController.java
+++ src/main/java/com/managefarming/powerinformerbackend/controllers/PowerInformerController.java
@@ -5,28 +5,16 @@ import com.managefarming.powerinformerbackend.entities.Farmer;
 import com.managefarming.powerinformerbackend.services.FarmerService;
 import com.managefarming.powerinformerbackend.services.HandleHeartBeatService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+
 public class PowerInformerController {
 
-    @Autowired
-    private HandleHeartBeatService handleHeartBeatService;
-
-    @Autowired
-    private FarmerService farmerService;
-
-
-    @RequestMapping("/device-heart-beat/{deviceId}/{authToken}")
-    public void handleDeviceHeartbeat(@PathVariable long deviceId, @PathVariable long authToken){
-        System.out.println("deviceId: " + deviceId);
-        System.out.println("AuthToken: " + authToken);
+    @RequestMapping(value="/", method = RequestMethod.GET)
+    public String getHomePage(){
+        return "hello there";
     }
 
-    @RequestMapping("/farmers/{farmerId}")
-    public Farmer getFarmersById(@PathVariable Long farmerId){
-        return farmerService.getFarmerByFarmerId(farmerId);
-    }
+
 }
