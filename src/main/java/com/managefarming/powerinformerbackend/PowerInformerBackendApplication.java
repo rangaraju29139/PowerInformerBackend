39458f1ae282a853f1e058d9d6546daea63ae0d2
rangaraju penmetsa <rangaraju29139@gmail.com>
2023-10-02 11:45:34 +0530

    added the post request to add farmers

--- src/main/java/com/managefarming/powerinformerbackend/PowerInformerBackendApplication.java
+++ src/main/java/com/managefarming/powerinformerbackend/PowerInformerBackendApplication.java
@@ -2,6 +2,7 @@ package com.managefarming.powerinformerbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class PowerInformerBackendApplication {
