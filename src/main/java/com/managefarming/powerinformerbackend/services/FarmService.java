52fa658eecba916ea762cb42804fce6e13cfb0c1
rangaraju penmetsa <rangaraju29139@gmail.com>
2023-10-05 17:05:59 +0530

    added the create Device Controller

--- src/main/java/com/managefarming/powerinformerbackend/services/FarmService.java
+++ src/main/java/com/managefarming/powerinformerbackend/services/FarmService.java
@@ -1,16 +1,22 @@
 package com.managefarming.powerinformerbackend.services;
 
 
+import com.managefarming.powerinformerbackend.DTO.device.DeviceDto;
+import com.managefarming.powerinformerbackend.DTO.device.mapper.DeviceDtoMapper;
 import com.managefarming.powerinformerbackend.DTO.farm.FarmResponseDto;
 import com.managefarming.powerinformerbackend.DTO.farm.mappers.FarmResponseDtoMapper;
 import com.managefarming.powerinformerbackend.DTO.farmer.mappers.FarmerResponseDtoMapper;
+import com.managefarming.powerinformerbackend.entities.Device;
 import com.managefarming.powerinformerbackend.entities.Farm;
 import com.managefarming.powerinformerbackend.entities.Farmer;
+import com.managefarming.powerinformerbackend.repositories.DeviceRepository;
 import com.managefarming.powerinformerbackend.repositories.FarmRepository;
 import com.managefarming.powerinformerbackend.repositories.FarmerRepository;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class FarmService {
@@ -19,6 +25,9 @@ public class FarmService {
     private FarmerRepository farmerRepository;
     @Autowired
     private FarmRepository farmRepository;
+
+    @Autowired
+    private DeviceRepository deviceRepository;
 
 
 
@@ -43,5 +52,12 @@ public class FarmService {
     }
 
 
-
+    public List<DeviceDto> getDevicesByFarmId(Long farmId) {
+        Farm farm = farmRepository.findById(farmId).get();
+        List<DeviceDto> deviceDtos = deviceRepository.findByFarm(farm).stream().map(device -> DeviceDtoMapper.maptoDeviceDto(device)).toList();
+        if(deviceDtos == null){
+            return null;
+        }
+        return deviceDtos;
+    }
 }
