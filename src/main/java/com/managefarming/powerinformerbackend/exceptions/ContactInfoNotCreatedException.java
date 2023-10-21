2caeee2abbc15006b0eafd6a411c274f642f416e
rangaraju penmetsa <rangaraju29139@gmail.com>
2023-10-21 11:04:27 +0530

    added the exceptions to return the response codes

--- src/main/java/com/managefarming/powerinformerbackend/exceptions/ContactInfoNotCreatedException.java
+++ src/main/java/com/managefarming/powerinformerbackend/exceptions/ContactInfoNotCreatedException.java
@@ -1,5 +1,10 @@
 package com.managefarming.powerinformerbackend.exceptions;
 
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 public class ContactInfoNotCreatedException extends RuntimeException {
     public ContactInfoNotCreatedException(String message) {
         super(message);
